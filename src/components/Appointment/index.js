54d711571012f45c20480f51f748a71ca9b31e2a
SamualM2021 <smartelli@zoho.com>
2021-12-14 13:40:47 -0500

    Added Appointment Booked and Empty stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,9 +9,21 @@ import Show from "./Show";
 import Status from "./Status";
 import "components/styles/Appointment.scss";
 
-const Appointment = () => {
+/**
+* Appointment Overview
+*  Appointment takes the following props
+* - time: A string representing the time of the appointment
+* - interview (Optional): An object representing the interview
+* @param {*} props
+* @returns
+*/
+const Appointment = props => {
+  const interview = props.interview;
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <h1>{props.time}</h1>
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+    </article>
   );
 }
 
